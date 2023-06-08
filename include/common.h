cdc5f7453bf243ddcdd032b694d6bc784cf762cd
endingly <jiasongdeng@qq.com>
2023-06-08 09:55:28 +0800

    1. add shared target and install it 2. add clang-format file 3. CMakeLists add a target compiler defined to build windows shared library

--- include/common.h
+++ include/common.h
@@ -1,6 +1,9 @@
+// clang-format off
 #pragma once
-#ifdef _WIN32
-#define CGL_EXPORT __declspec(dllexport)
-#else
-#define CGL_EXPORT
+    #ifdef _WIN32
+        #ifdef BUILD_SHARED_LIBS
+            #define CGL_EXPORT __declspec(dllexport)
+        #else
+            #define CGL_EXPORT
+        #endif
 #endif
